86292913006a6200f7747b6ab8c17e8e742bd284
subbarao dv <subbarao dv>
2017-02-11 20:25:20 +0530

    assignment4

--- src/home/menudata.service.js
+++ src/home/menudata.service.js
@@ -10,7 +10,28 @@ MenuDataService.$inject = ['$q', '$timeout']
 function MenuDataService($q, $timeout) {
   var service = this;
 
+  // List of shopping items
+  var items = [];
 
+  // Pre-populate a no cookie list
+  items.push({
+    name: "Sugar",
+    quantity: "2 bags",
+    description: "Sugar used for baking delicious umm... baked goods."
+  });
+  items.push({
+    name: "flour",
+    quantity: "1 bags",
+    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
+  });
+  items.push({
+    name: "Chocolate Chips",
+    quantity: "3 bags",
+    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
+  });
+
+  // Simulates call to server
+  // Returns a promise, NOT items array directly
   service.getItems = function () {
     var deferred = $q.defer();
 
@@ -18,7 +39,7 @@ function MenuDataService($q, $timeout) {
     $timeout(function () {
       // deferred.reject(items);
       deferred.resolve(items);
-    }, 1500);
+    }, 800);
 
     return deferred.promise;
   };
